35cb10fa1a40f2520b90a6b5032dedc05c8a8444
Mateus Sales Albino <mateus3009@gmail.com>
2024-03-19 20:06:45 -0300

    Feat: Saving the customer during sign-up

--- src/sign-up/lambda_function.py
+++ src/sign-up/lambda_function.py
@@ -2,108 +2,64 @@ import json
 import logging
 import os
 
-import boto3
-import botocore.exceptions
-import psycopg2
 import requests
+import boto3
 
-cognito_client = boto3.client('cognito-idp')
+cognito_client = boto3.client("cognito-idp")
 
-USER_POOL_ID = os.environ.get('USER_POOL_ID')
-
-headers = {'X-Aws-Parameters-Secrets-Token': os.environ.get('AWS_SESSION_TOKEN')}
-params_extension_endpoint = 'http://localhost:2773/systemsmanager/parameters/get?name='
-secrets_extension_endpoint = 'http://localhost:2773/secretsmanager/get?secretId='
-
-
-def get_param(param_name):
-    return json.loads(requests.get(params_extension_endpoint + param_name, headers=headers).text)
-
-
-def get_secret(secret_id):
-    return json.loads(requests.get(secrets_extension_endpoint + secret_id, headers=headers).text)
-
-
-def internal_error(error):
-    logging.error(error)
-    return {
-        'statusCode': 500,
-        'headers': {'Content-Type': 'application/json'},
-        'body': "{ 'message': 'Internal server error' }",
-    }
+USER_POOL_ID = os.environ.get("USER_POOL_ID")
+LOAD_BALANCER_DNS = os.environ.get("LOAD_BALANCER_DNS")
+TARGET_PORT = os.environ.get("TARGET_PORT")
 
 
 def lambda_handler(event, context):
-    body = json.loads(event.get('body', '{}'))
+    logging.info(event)
 
-    email = body.get('email')  # TODO: validate email address
-    name = body.get('name')  # TODO: validate name
-    cpf = body.get('cpf')  # TODO: validate CPF
+    body = json.loads(event.get("body", "{}"))
+
+    email = body.get("email")
+    name = body.get("name")
+    cpf = body.get("cpf")
 
     user_attributes = []
+    payload = {}
+
     if cpf:
         username = cpf
-        user_attributes.append({'Name': 'custom:CPF', 'Value': cpf})
+        user_attributes.append({"Name": "custom:CPF", "Value": cpf})
+        payload["document"] = cpf
     elif email and name:
         username = email
-        user_attributes.extend([
-            {'Name': 'email', 'Value': email},
-            {'Name': 'email_verified', 'Value': 'true'},
-            {'Name': 'name', 'Value': name},
-        ])
+        user_attributes.extend(
+            [
+                {"Name": "email", "Value": email},
+                {"Name": "email_verified", "Value": "true"},
+                {"Name": "name", "Value": name},
+            ]
+        )
+        payload["email"] = email
+        payload["name"] = name
     else:
         return {
-            'statusCode': 400,
-            'headers': {'Content-Type': 'application/json'},
-            'body': "{ 'message': 'Please provide either CPF or both Email and Name' }"
+            "statusCode": 400,
+            "headers": {"Content-Type": "application/json"},
+            "body": "{ 'message': 'Please provide either CPF or both Email and Name' }",
         }
 
-    try:
-        RDS_PARAMS = get_param(os.environ.get('RDS_PARAM_ID'))
-        RDS_SECRETS = get_secret(os.environ.get('RDS_SECRET_ID'))
+    headers = {"Content-Type": "application/json"}
+    url = f"https://{LOAD_BALANCER_DNS}:{TARGET_PORT}/live/customers"
+    customer_id = requests.post(url, json=payload, headers=headers).json()["id"]
+    user_attributes.append({"Name": "custom:CUSTOMER_ID", "Value": customer_id})
 
-        response = cognito_client.admin_create_user(
-            UserPoolId=USER_POOL_ID,
-            Username=username,
-            UserAttributes=user_attributes,
-            MessageAction='SUPPRESS'
-        )
+    response = cognito_client.admin_create_user(
+        UserPoolId=USER_POOL_ID,
+        Username=username,
+        UserAttributes=user_attributes,
+        MessageAction="SUPPRESS",
+    )
+    logging.info(response)
 
-        logging.info(response)
-
-        response = cognito_client.admin_add_user_to_group(
-            UserPoolId=USER_POOL_ID,
-            Username=username,
-            GroupName='customer'
-        )
-
-        logging.info(response)
-
-        # try:
-        #     conn = psycopg2.connect(
-        #         user=RDS_SECRETS['username'], password=RDS_SECRETS['password'],
-        #         host=RDS_PARAMS['endpoint'].split(':')[0], port=RDS_PARAMS['port'], dbname=RDS_PARAMS['name']
-        #     )
-        # except psycopg2.Error as error:
-        #     internal_error(error)
-
-        return {
-            'statusCode': 200,
-            'headers': {'Content-Type': 'application/json'},
-            'body': "{ 'message': 'User created successfully' }"
-        }
-
-    except botocore.exceptions.ClientError as error:
-        logging.error(error)
-
-        if error.response['Error']['Code'] == 'UsernameExistsException':
-            return {
-                'statusCode': 403,
-                'headers': {'Content-Type': 'application/json'},
-                'body': "{ 'message': 'Unauthorized' }"
-            }
-
-        return internal_error(error)
-
-    except Exception as error:
-        return internal_error(error)
+    response = cognito_client.admin_add_user_to_group(
+        UserPoolId=USER_POOL_ID, Username=username, GroupName="customer"
+    )
+    logging.info(response)
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,15 @@ variable "tags" {
     managed_by_terraform = true
   }
 }
+
+variable "load_balancer_name" {
+  description = "The name of the load balancer"
+  type        = string
+  default     = "self-order-management-lb"
+}
+
+variable "target_group_port" {
+  description = "The port of the target group"
+  type        = number
+  default     = 80
+}
